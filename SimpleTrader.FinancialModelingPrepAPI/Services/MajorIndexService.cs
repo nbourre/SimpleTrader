ca1a5a061d0b457cb2db4a1b331eae9beba497e1
Nicolas Bourré <nbourrereg@hotmail.com>
2021-10-24 18:52:00 -0400

    WiP : Search for TODO

---
+++ SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
@@ -0,0 +1,23 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrepAPI.Services
+{
+    public class MajorIndexService : IMajorIndexService
+    {
+        public Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                /// TODO : Rendu https://www.youtube.com/watch?v=0SCKUine6tY&list=PLA8ZIAm2I03jSfo18F7Y65XusYzDusYu5&index=4&t=6m00s
+                ///await client.GetAsync()
+            }
+        }
+    }
+}
